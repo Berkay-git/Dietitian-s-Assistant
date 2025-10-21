d8a49c17c9fafe27edc4d0ff4bb9357d824e9969
Berkay-git <berkayceylan13@gmail.com>
2025-10-21 14:21:24 +0300

    Template with DB

---
+++ Flask_BackEnd/app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask_cors import CORS
+from db_config import db
+from flask_migrate import Migrate
+import os
+
+# dietisyen için için yeni blueprint yazıcaz 
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)  
+
+    # Configuration
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY') or 'dev-fallback-key'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
+        'mysql+pymysql://root:1234@localhost/dietitian_assistant'  # MySQL buraya kendi database şifrnizi yazıcaksınız şu 1234 olan kısıma
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    
+    db.init_app(app)
+    migrate = Migrate(app, db)
+
+    # Register blueprints buraya gelicek BP ler
+
+    return app
+
+if __name__ == '__main__':
+    app = create_app()
+    print(app.url_map)
+    app.run(debug=True)
